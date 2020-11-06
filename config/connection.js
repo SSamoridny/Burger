c4b33b3b4621829c3b0ef25fed739070ea2a94c1
Shaun Samoridny <ssamoridny@gmail.com>
2020-11-06 15:00:54 -0700

    partial set up of files and connections

---
+++ config/connection.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const mysql = require( 'mysql' );
+
+const defaultConnecton = {
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+};
+
+
+const connectionString = process.env.JAWSDB_URL || defaultConnecton;
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args=[] ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err ) {
+                    return reject( err );
+                }
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err ) {
+                    return reject( err );
+                }
+                resolve();
+            } );
+        } );
+    }
+}
+
+const db = new Database(connectionString);
+
+module.exports = db;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+insert into burgers(burger_name) values ("Royale With Cheese");
+insert into burgers(burger_name) values ("The Good Burger");
+insert into burgers(burger_name) values ("The Dad Burger");
