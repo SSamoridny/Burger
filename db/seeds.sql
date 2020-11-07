dae298a6ba1c8c8db2c4eb40e814be061f6204c6
Shaun Samoridny <ssamoridny@gmail.com>
2020-11-07 11:51:18 -0700

    added css styling and began handlebars section, added the NPM dependencies

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
-insert into burgers(burger_name) values ("Royale With Cheese");
-insert into burgers(burger_name) values ("The Good Burger");
-insert into burgers(burger_name) values ("The Dad Burger");
+insert into burgers(burger_name, devoured) values ("Royale With Cheese", true);
+insert into burgers(burger_name, devoured) values ("The Good Burger", false);
+insert into burgers(burger_name, devoured) values ("The Dad Burger", false);
